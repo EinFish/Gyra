84c5c52b91829908e20371946b3726da88ef8fb6
EinFish <finley.alster2@gmail.com>
2023-11-01 20:25:46 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!-- HTML Part -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/index.css">
+    <title>Home - Gyra</title>
+</head>
+<body>
+    <h1>Work in Progress!</h1> 
+</body>
+</html>
